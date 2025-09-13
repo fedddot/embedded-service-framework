f55c902542578b22b78b157b12801c25c37a8b68
fedddot <idanrommm@gmail.com>
2025-09-13 11:59:15 +0000

    host impl updated

--- host/include/host.hpp
+++ host/include/host.hpp
@@ -14,16 +14,16 @@ namespace host {
 	template <typename ApiRequest, typename ApiResponse>
 	class Host {
 	public:
-		using ApiRequestReader = ipc::DataReader<std::optional<ApiRequest>(void)>;
+		using ApiRequestReader = ipc::DataReader<std::optional<ApiRequest>>;
 		using ApiResponseWriter = ipc::DataWriter<ApiResponse>;
 		using FailureReporter = std::function<ApiResponse(const std::exception&)>;
 		using ServiceInstance = service::Service<ApiRequest, ApiResponse>;
 
 		Host(
-			const ApiRequestReader *api_request_reader_ptr,
-			const ApiResponseWriter *api_response_writer_ptr,
-			const FailureReporter& failure_reporter,
-			ServiceInstance *service_ptr
+			ApiRequestReader *api_request_reader_ptr,
+			ApiResponseWriter *api_response_writer_ptr,
+			ServiceInstance *service_ptr,
+			const FailureReporter& failure_reporter
 		);
 		Host(const Host&) = default;
 		Host& operator=(const Host&) = default;
@@ -31,19 +31,19 @@ namespace host {
 		
 		void run_once();
 	private:
-		const ApiRequestReader *m_api_request_reader_ptr;
-		const ApiResponseWriter *m_api_response_writer_ptr;
-		FailureReporter m_failure_reporter;
+		ApiRequestReader *m_api_request_reader_ptr;
+		ApiResponseWriter *m_api_response_writer_ptr;
 		ServiceInstance *m_service_ptr;
+		FailureReporter m_failure_reporter;
 	};
 
 	template <typename ApiRequest, typename ApiResponse>
 	inline Host<ApiRequest, ApiResponse>::Host(
-		const ApiRequestReader *api_request_reader_ptr,
-		const ApiResponseWriter *api_response_writer_ptr,
-		const FailureReporter& failure_reporter,
-		ServiceInstance *service_ptr
-	): m_api_request_reader_ptr(api_request_reader_ptr), m_api_response_writer_ptr(api_response_writer_ptr), m_failure_reporter(failure_reporter), m_service_ptr(service_ptr) {
+			ApiRequestReader *api_request_reader_ptr,
+			ApiResponseWriter *api_response_writer_ptr,
+			ServiceInstance *service_ptr,
+			const FailureReporter& failure_reporter
+	): m_api_request_reader_ptr(api_request_reader_ptr), m_api_response_writer_ptr(api_response_writer_ptr), m_service_ptr(service_ptr), m_failure_reporter(failure_reporter) {
 		if (!m_api_request_reader_ptr || !m_api_response_writer_ptr || !m_failure_reporter || !m_service_ptr) {
 			throw std::invalid_argument("invalid args received in Host ctor");
 		}
