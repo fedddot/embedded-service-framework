3f94277fdd772030fbd71e7d86935bead7266518
fedddot <idanrommm@gmail.com>
2025-09-19 16:34:15 +0000

    renamed

--- ipc/package/include/package_reader.hpp
+++ ipc/package/include/package_reader.hpp
@@ -16,7 +16,7 @@ namespace ipc {
 	public:
 		using SizeRetriever = std::function<std::size_t(const InputStream<std::uint8_t>&)>;
 		PackageReader(
-			InputStream<std::uint8_t> *queue_ptr,
+			InputStream<std::uint8_t> *byte_stream_ptr,
 			const SizeRetriever& size_retriever,
 			const std::size_t& header_size
 		);
@@ -24,42 +24,42 @@ namespace ipc {
 		PackageReader& operator=(const PackageReader&) = delete;
 		std::optional<std::vector<std::uint8_t>> read() override;
 	private:
-		InputStream<std::uint8_t> *m_queue_ptr;
+		InputStream<std::uint8_t> *m_byte_stream_ptr;
 		SizeRetriever m_size_retriever;
 		std::size_t m_header_size;
 	};
 
 	inline PackageReader::PackageReader(
-		InputStream<std::uint8_t> *queue_ptr,
+		InputStream<std::uint8_t> *byte_stream_ptr,
 		const SizeRetriever& size_retriever,
 		const std::size_t& header_size
-	): m_queue_ptr(queue_ptr), m_size_retriever(size_retriever), m_header_size(header_size) {
-		if (!m_queue_ptr || !m_size_retriever) {
+	): m_byte_stream_ptr(byte_stream_ptr), m_size_retriever(size_retriever), m_header_size(header_size) {
+		if (!m_byte_stream_ptr || !m_size_retriever) {
 			throw std::invalid_argument("invalid args in package reader received");
 		}
 	}
 
 	inline std::optional<std::vector<std::uint8_t>> PackageReader::read() {
 		try {
-			if (m_queue_ptr->size() < m_header_size) {
+			if (m_byte_stream_ptr->size() < m_header_size) {
 				return std::nullopt;
 			}
-			const auto package_size = m_size_retriever(*m_queue_ptr);
-			if (m_queue_ptr->size() < package_size + m_header_size) {
+			const auto package_size = m_size_retriever(*m_byte_stream_ptr);
+			if (m_byte_stream_ptr->size() < package_size + m_header_size) {
 				return std::nullopt;
 			}
 			std::vector<std::uint8_t> package_data(package_size, 0);
 			for (std::size_t i = 0; i < package_size; ++i) {
-				package_data[i] = m_queue_ptr->inspect(i + m_header_size);
+				package_data[i] = m_byte_stream_ptr->inspect(i + m_header_size);
 			}
 			auto bytes_to_discard = m_header_size + package_size;
 			while (bytes_to_discard) {
-				m_queue_ptr->read();
+				m_byte_stream_ptr->read();
 				--bytes_to_discard;
 			}
 			return package_data;
 		} catch (...) {
-			m_queue_ptr->clear();
+			m_byte_stream_ptr->clear();
 			throw;
 		}
 	}
