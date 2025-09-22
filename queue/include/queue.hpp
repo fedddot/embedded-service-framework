291f0cb0171ab1a601270a98b739682751e5fbca
fedddot <idanrommm@gmail.com>
2025-09-22 09:47:54 +0000

    queue added

---
+++ queue/include/queue.hpp
@@ -0,0 +1,19 @@
+#ifndef	QUEUE_HPP
+#define	QUEUE_HPP
+
+#include <cstddef>
+
+namespace queue {
+	template <typename T>
+	class Queue {
+	public:
+		virtual ~Queue() noexcept = default;
+		virtual void enqueue(const T& item) = 0;
+		virtual T dequeue() = 0;
+		virtual const T& inspect(const std::size_t index) const = 0;
+		virtual std::size_t size() const = 0;
+		virtual void clear() = 0;
+	};
+}
+
+#endif // QUEUE_HPP
