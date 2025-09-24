c8d225d1e50c8dbe94b6a83eba6e1fd16f58dc3e
fedddot <idanrommm@gmail.com>
2025-09-24 21:43:33 +0000

    test updated

--- service/asynchronous_drive_service/tests/src/ut_asynchronous_drive_service.cpp
+++ service/asynchronous_drive_service/tests/src/ut_asynchronous_drive_service.cpp
@@ -1,14 +1,23 @@
 #include "gtest/gtest.h"
+#include <cstddef>
 
+#include "asynchronous_drive_request.hpp"
 #include "asynchronous_drive_service.hpp"
 
 using namespace service;
 
 TEST(ut_asynchronous_drive_service, run_api_request_sanity) {
 	// GIVEN
+	const auto test_speed_rpm = std::size_t(150);
+	const auto test_start_request = AsynchronousDriveRequest(
+		AsynchronousDriveRequest::Type::START,
+		test_speed_rpm
+	);
 	
 	// WHEN
 	AsynchronousDriveService service;
+	const auto response = service.run_api_request(test_start_request);
 	
 	// THEN
+	EXPECT_EQ(response.result(), AsynchronousDriveResponse::Result::SUCCESS);
 }
