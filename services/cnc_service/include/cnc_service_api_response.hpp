dd73b859f7be53ad185baf604d7e12b0b35476b3
fedddot <idanrommm@gmail.com>
2025-09-25 12:17:17 +0000

    response updated

--- services/cnc_service/include/cnc_service_api_response.hpp
+++ services/cnc_service/include/cnc_service_api_response.hpp
@@ -3,31 +3,27 @@
 
 #include <functional>
 #include <optional>
+#include <string>
 
 namespace service {
-	template <typename RouteId, typename Payload>
 	class CncServiceApiResponse {
 	public:
 		enum class Result: int {
 			SUCCESS = 0,
-			UNSUPPORTED_ROUTE = 1,
-			UNEXPECTED_FAILURE = 2
+			FAILURE = 1,
 		};
 		CncServiceApiResponse(
 			const Result& result,
-			const RouteId& route_id,
-			const std::optional<Payload>& payload
-		): m_result(result), m_route_id(route_id), m_payload(payload) {}
+			const std::optional<std::string>& message = std::nullopt
+		): m_result(result), m_message(message) {}
 		CncServiceApiResponse(const CncServiceApiResponse&) = default;
 		CncServiceApiResponse& operator=(const CncServiceApiResponse&) = default;
 		virtual ~CncServiceApiResponse() noexcept = default;
 		const Result& result() const { return std::ref(m_result); }
-		const RouteId& route_id() const { return std::ref(m_route_id); }
-		const std::optional<Payload>& payload() const { return std::ref(m_payload); }
+		const std::optional<std::string>& message() const { return std::ref(m_message); }
 	private:
 		Result m_result;
-		RouteId m_route_id;
-		std::optional<Payload> m_payload;
+		std::optional<std::string> m_message;
 	};
 }
 
