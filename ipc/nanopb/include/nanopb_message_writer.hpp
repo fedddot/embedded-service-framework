4fa42a10389691cb9f36e45f784f91bb6dccff09
fedddot <idanrommm@gmail.com>
2025-10-03 14:06:57 +0000

    implemented

--- ipc/nanopb/include/nanopb_message_writer.hpp
+++ ipc/nanopb/include/nanopb_message_writer.hpp
@@ -13,36 +13,41 @@
 
 namespace ipc {
 	template <typename ApiMessage, typename NanoPbMessage, std::size_t MAX_MSG_SIZE>
-	class ApiMessageWriter: public DataWriter<ApiMessage> {
+	class NanopbMessageWriter: public DataWriter<ApiMessage> {
 	public:
 		using PackageWriter = DataWriter<std::vector<std::uint8_t>>;
 		using ApiMessageSerializer = std::function<NanoPbMessage(const ApiMessage&)>;
-		ApiMessageWriter(
+		using NanoPbMessageDeinitFunc = std::function<void(NanoPbMessage *)>;
+		NanopbMessageWriter(
 			PackageWriter *package_writer_ptr,
 			const ApiMessageSerializer& message_serializer,
+			const NanoPbMessageDeinitFunc& deinit_nanopb_message,
 			const pb_msgdesc_t *nanopb_message_fields
-		): m_package_writer_ptr(package_writer_ptr), m_message_serializer(message_serializer), m_nanopb_message_fields(nanopb_message_fields) {
+		): m_package_writer_ptr(package_writer_ptr), m_message_serializer(message_serializer), m_deinit_nanopb_message(deinit_nanopb_message), m_nanopb_message_fields(nanopb_message_fields) {
 			if (!m_package_writer_ptr || !m_message_serializer) {
 				throw std::invalid_argument("invalid args received");
 			}
 		}
-		ApiMessageWriter(const ApiMessageWriter&) = default;
-		ApiMessageWriter& operator=(const ApiMessageWriter&) = delete;
+		NanopbMessageWriter(const NanopbMessageWriter&) = default;
+		NanopbMessageWriter& operator=(const NanopbMessageWriter&) = delete;
 		void write(const ApiMessage& message) override {
-			const auto pb_message = m_message_serializer(message);
+			auto pb_message = m_message_serializer(message);
 			pb_byte_t buff[MAX_MSG_SIZE] = { 0 };
 			pb_ostream_t ostream = pb_ostream_from_buffer(
                 buff,
                 MAX_MSG_SIZE
             );
             if (!pb_encode(&ostream, m_nanopb_message_fields, &pb_message)) {
+				m_deinit_nanopb_message(&pb_message);
                 throw std::runtime_error("failed to encode api message into protocol buffer: " + std::string(PB_GET_ERROR(&ostream)));
             }
+			m_deinit_nanopb_message(&pb_message);
             m_package_writer_ptr->write(std::vector<std::uint8_t>((const char *)buff, (const char *)buff + ostream.bytes_written));
 		}
 	private:
 		PackageWriter *m_package_writer_ptr;
 		const ApiMessageSerializer m_message_serializer;
+		NanoPbMessageDeinitFunc m_deinit_nanopb_message;
 		const pb_msgdesc_t *m_nanopb_message_fields;
 	};
 }
--- ipc/nanopb/tests/src/ut_nanopb_message_writer.cpp
+++ ipc/nanopb/tests/src/ut_nanopb_message_writer.cpp
@@ -1,4 +1,7 @@
+#include <cstddef>
 #include <cstdint>
+#include <optional>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -6,6 +9,10 @@
 #include "gmock/gmock.h"
 
 #include "nanopb_message_writer.hpp"
+#include "data_writer.hpp"
+#include "pb.h"
+#include "pb_encode.h"
+#include "test.pb.h"
 
 using namespace ipc;
 
@@ -16,44 +23,84 @@ public:
     MOCK_METHOD(void, write, (const std::vector<std::uint8_t>&), (override));
 };
 
-TEST(ut_nanopb_message_writer, ctor_dtor_sanity) {
-	// GIVEN
-	auto package_writer = MockPackageWriter();
-	const auto message_serializer = [](const ApiMessage& message) -> std::vector<std::uint8_t> {
-		throw std::runtime_error("not implemented");
-	};
-
-	// WHEN
-	ApiMessageWriter<ApiMessage> *instance = nullptr;
-
-
-	// THEN
-	ASSERT_NO_THROW(
-		instance = new ApiMessageWriter<ApiMessage>(
-			&package_writer,
-			message_serializer
-		)
-	);
-	ASSERT_NO_THROW(delete instance);
-	instance = nullptr;
-}
+static test_api_TestRequest serialize_api_message(const ApiMessage& msg);
+static bool decode_string(pb_istream_t *stream, const pb_field_t *field, void **arg);
+static bool encode_string(pb_ostream_t *, const pb_field_t *, void *const *);
 
 TEST(ut_nanopb_message_writer, write_sanity) {
 	// GIVEN
-	const auto nanopb_message = ApiMessage("test_msg");
+	const auto test_api_message = ApiMessage("test_msg");
 	auto package_writer = ::testing::NiceMock<MockPackageWriter>();
-    EXPECT_CALL(package_writer, write(::testing::_)).Times(1);
-	const auto message_serializer = [](const ApiMessage& message) -> std::vector<std::uint8_t> {
-		return std::vector<std::uint8_t>(message.begin(), message.end());
+	const auto serializer = [](const ApiMessage& data) -> test_api_TestRequest {
+		test_api_TestRequest pb_request(test_api_TestRequest_init_default);
+		pb_request.request.arg = new std::string(data);
+		pb_request.request.funcs.encode = encode_string;
+		return pb_request;
+	};
+	const auto init_pb_msg = []() -> test_api_TestRequest {
+		char buff[256] = {'\0'};
+		test_api_TestRequest pb_message = test_api_TestRequest_init_default;
+		pb_message.request.funcs.decode = decode_string;
+		pb_message.request.arg = new std::string();
+		return pb_message;
+	};
+	const auto deinit_pb_msg = [](test_api_TestRequest *pb_request) {
+		delete (std::string *)((pb_request->request).arg);
 	};
 
-	
 	// WHEN
-	ApiMessageWriter<ApiMessage> instance(
+	NanopbMessageWriter<ApiMessage, test_api_TestRequest, 256UL> instance(
 		&package_writer,
-		message_serializer
+		message_parser,
+		init_pb_msg,
+		deinit_pb_msg,
+		test_api_TestRequest_fields
 	);
+	auto result = std::optional<ApiMessage>();
 
 	// THEN
-	ASSERT_NO_THROW(instance.write(nanopb_message));
+	ASSERT_NO_THROW(result = instance.write());
+	ASSERT_TRUE(result.has_value());
+	ASSERT_EQ(*result, test_api_message);
+}
+
+static bool encode_string(pb_ostream_t *stream, const pb_field_t *field, void * const *arg);
+inline std::vector<std::uint8_t> serialize_api_message(const ApiMessage& msg) {
+	test_api_TestRequest api_request(test_api_TestRequest_init_default);
+	api_request.request.funcs.encode = encode_string;
+	api_request.request.arg = const_cast<char *>(msg.c_str());
+	enum: std::size_t { BUFF_SIZE = 256UL };
+	pb_byte_t buff[BUFF_SIZE] = { '\0' };
+	pb_ostream_t ostream = pb_ostream_from_buffer(buff, BUFF_SIZE);
+	if (!pb_encode(&ostream, test_api_TestRequest_fields, &api_request)) {
+		throw std::runtime_error("failed to encode api message");
+	}
+	return std::vector<std::uint8_t>(static_cast<const std::uint8_t *>(buff), static_cast<const std::uint8_t *>(buff + ostream.bytes_written));
+}
+
+inline bool encode_string(pb_ostream_t *stream, const pb_field_t *field, void * const *arg) {
+	if (!arg || !*arg) {
+		throw std::runtime_error("encode_string called with null arg");
+	}
+	const auto str = static_cast<const char *>(*arg);
+	if (!pb_encode_tag_for_field(stream, field)) {
+		return false;
+	}
+	return pb_encode_string(stream, (const pb_byte_t *)(str), std::strlen(str));
+}
+
+inline bool decode_string(pb_istream_t *stream, const pb_field_t *field, void **arg) {
+	if (!arg) {
+		throw std::runtime_error("encode_string called with null arg");
+	}
+	std::string *dst = *(std::string **)(arg);
+    if (!dst) {
+		throw std::runtime_error("destination buffer is not set");
+	}
+	pb_byte_t buff[256UL] = {'\0'};
+	if (!pb_write(stream, buff, stream->bytes_left)) {
+		return false;
+	}
+	*dst = std::string((const char *)buff);
+    return true;
 }
