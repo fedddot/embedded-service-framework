c0a55d9e910a1ed39ec228cf07b19fc1a5f0bada
fedddot <idanrommm@gmail.com>
2025-10-04 08:42:37 +0000

    req implemented

--- service/motor_drive_service/include/motor_drive_service_api_request.hpp
+++ service/motor_drive_service/include/motor_drive_service_api_request.hpp
@@ -12,18 +12,25 @@ namespace service {
 			STOP,
 			STATUS
 		};
+		enum class Direction: int {
+			CW,
+			CCW
+		};
 		MotorDriveServiceApiRequest(
 			const Type& type,
-			const std::optional<double>& speed
-		): m_type(type), m_speed(speed) {}
+			const std::optional<double>& speed = std::nullopt,
+			const std::optional<Direction>& direction = std::nullopt
+		): m_type(type), m_speed(speed), m_direction(direction) {}
 		MotorDriveServiceApiRequest(const MotorDriveServiceApiRequest&) = default;
 		MotorDriveServiceApiRequest& operator=(const MotorDriveServiceApiRequest&) = default;
 		virtual ~MotorDriveServiceApiRequest() noexcept = default;
 		const Type& type() const { return std::ref(m_type); }
 		const std::optional<double>& speed() const { return std::ref(m_speed); }
+		const std::optional<Direction>& direction() const { return std::ref(m_direction); }
 	private:
 		Type m_type;
 		std::optional<double> m_speed;
+		std::optional<Direction> m_direction;
 	};
 }
 
